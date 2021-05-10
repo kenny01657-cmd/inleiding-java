2d4ae78b49b37391a9a2540ed6844782e69788e2
kenny01657-cmd <kmeijer@roc-dev.com>
2021-05-10 13:04:18 +0200

    created an applet that shows a blue screen withe my name in yellow

---
+++ src/h02/FirstApplet.java
@@ -0,0 +1,20 @@
+package h02;
+
+// importeer de benodigde klassen uit de java bibliotheek
+import java.applet.Applet;
+import java.awt.*;
+
+// een klasse met de naam FirstApplet van het type applet
+public class FirstApplet extends Applet {
+
+    // een (lege) methode die de applet initialiseert
+    public void init() {
+        setBackground(Color.blue);
+    }
+
+    // een methode die de inhoud van het scherm tekent
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.drawString("Kenny Meijer", 50, 60);
+    }
+}
