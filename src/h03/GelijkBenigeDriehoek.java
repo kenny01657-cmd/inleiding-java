a0980d1d4f4cfcd6c709b24e43c283c0b1087372
kenny01657-cmd <kmeijer@roc-dev.com>
2021-05-10 15:26:42 +0200

    This program draws a isosceles triangle

---
+++ src/h03/GelijkBenigeDriehoek.java
@@ -0,0 +1,19 @@
+package h03;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class GelijkBenigeDriehoek extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g){
+        // house
+        g.drawLine(100,120,250,120);
+        g.drawLine(100,120,175,20);
+        g.drawLine(250,120,175,20);
+
+    }
+}
