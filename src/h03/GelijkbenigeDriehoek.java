aedbbda642641f49349f356fac5dcb904e9ca509
kenny01657-cmd <kmeijer@roc-dev.com>
2021-05-10 15:22:36 +0200

    This program draws a house with a window and door

--- src/h03/GelijkbenigeDriehoek.java
+++ src/h03/GelijkbenigeDriehoek.java
@@ -10,9 +10,17 @@ public class GelijkbenigeDriehoek extends Applet {
     }
 
     public void paint(Graphics g){
-        // gelijkbenige driehoek
-        g.drawLine(150,120,190,120);
-        g.drawLine(150,120,170,85);
-        g.drawLine(190,120,170,85);
+        // house
+        g.drawLine(100,120,250,120);
+        g.drawLine(100,120,175,20);
+        g.drawLine(250,120,175,20);
+        g.drawRect(100,120,150,150);
+        // window
+        g.drawRect(110,150,40,40);
+        g.drawLine(110,170,150,170);
+        g.drawLine(130,190,130,150);
+        // door
+        g.drawRect(160,220,30,50);
+        g.drawRect(165,245,2,2);
     }
 }
