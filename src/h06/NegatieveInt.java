0ff69c51b5717e4f126634ce0d67d4acc8a9fb1e
kenny01657-cmd <kmeijer@roc-dev.com>
2021-05-12 11:59:21 +0200

    een applet waarin twee positieve int's bij elkaar worden opgetelt en er een negatieve int uitkomt

---
+++ src/h06/NegatieveInt.java
@@ -0,0 +1,22 @@
+package h06;
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class NegatieveInt extends Applet {
+int positiveInt;
+int positiveInt2;
+int uitkomst;
+
+
+    public void init() {
+        positiveInt = 2147483647;
+        positiveInt2 = 1;
+        uitkomst = positiveInt + positiveInt2;
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("De uitkomst is: " + uitkomst,20,20);
+    }
+}
