d1ac87b4d56add09e62a6388e0545f0d1ef05ad5
kenny01657-cmd <kmeijer@roc-dev.com>
2021-05-12 10:23:17 +0200

    de praktijk opdracht van hooftstuk 4 maar met variabele

--- src/h05/ShapesWithVariables.java
+++ src/h05/ShapesWithVariables.java
@@ -5,51 +5,132 @@ import java.applet.*;
 
 public class ShapesWithVariables extends Applet {
     // declaratie
+
     // kleur
     Color opvulkleur;
     Color lijnkleur;
+
     // breedte en hoogte
     int breedte;
     int hoogte;
-    // lijn coördinaten
+
+    // lijn
     int LijnX1;
     int LijnY1;
     int LijnX2;
     int LijnY2;
-    // lijn string coördinaten
     int LijnStringX;
     int LijnStringY;
-    // rechthoek coördinaten
+
+    // rechthoek
     int RHX;
     int RHY;
-    // rechthoek string coördinaten
     int RHSX;
     int RHSY;
+
+    // gevulde rechtgoek met ovaal
+    int GRX;
+    int GRY;
+    int OvalX;
+    int OvalY;
+    int StringX;
+    int StringY;
+
+    // Taartpunt met ovaal eromheen
+    int OvaalX;
+    int OvaalY;
+    int TaartPuntX;
+    int TaartPuntY;
+    int TPStartAngle;
+    int TPArcAngle;
+    int TPStringX;
+    int TPstringY;
+
+    // Afgeronde rechthoek
+    int ARX;
+    int ARY;
+    int ArcWidth;
+    int ArcHeight;
+    int StrX;
+    int StrY;
+
+    // Gevulde ovaal
+    int GevuldeOvaalX;
+    int GevuldeOvaalY;
+    int StrX1;
+    int StrY1;
+
+    // Cirkel
+    int CirkelX;
+    int CirkelY;
+    int CirkelStringX;
+    int CirkelStringY;
 
 
 
     public void init() {
         // initialiseren
+
         // kleur
         opvulkleur = Color.magenta;
         lijnkleur = Color.black;
+
         // hoogte en breedte
         breedte = 200;
         hoogte = 100;
-        // lijn coördinaten
+
+        // lijn
         LijnX1 = 10;
         LijnY1 = 10;
         LijnX2 = 200;
         LijnY2 = 10;
-        // lijn string coördinaten
-        LijnStringX = 85;
+        LijnStringX = 90;
         LijnStringY = 23;
-        // rechthoek coördinaten
+
+        // rechthoek
         RHX = 10;
         RHY = 50;
-        // rechthoek string coördinaten
         RHSX = 75;
         RHSY = 165;
+
+        // gevulde rechtgoek met ovaal
+        GRX = 250;
+        GRY = 50;
+        OvalX = 250;
+        OvalY = 50;
+        StringX = 280;
+        StringY = 165;
+
+        // Taartpunt met ovaal eromheen
+        OvaalX = 480;
+        OvaalY = 50;
+        TaartPuntX = 480;
+        TaartPuntY = 50;
+        TPStartAngle = 0;
+        TPArcAngle = 45;
+        TPStringX = 500;
+        TPstringY = 165;
+
+        // Afgeronde rechthoek
+        ARX = 10;
+        ARY = 200;
+        ArcWidth = 30;
+        ArcHeight = 30;
+        StrX = 50;
+        StrY = 315;
+
+        // Gevulde ovaal
+        GevuldeOvaalX = 250;
+        GevuldeOvaalY = 200;
+        StrX1 = 315;
+        StrY1 = 315;
+
+        // Cirkel
+        CirkelX = 525;
+        CirkelY = 200;
+        CirkelStringX = 560;
+        CirkelStringY = 315;
+
     }
 
 
@@ -66,30 +147,30 @@ public class ShapesWithVariables extends Applet {
 
         // Gevulde rechthoek met ovaal
         g.setColor(opvulkleur);
-        g.fillRect(250,50,breedte,hoogte);
+        g.fillRect(GRX,GRY,breedte,hoogte);
         g.setColor(lijnkleur);
-        g.drawOval(250,50,breedte,hoogte);
-        g.drawString("Gevulde rechthoek met ovaal",280,165);
+        g.drawOval(OvalX,OvalY,breedte,hoogte);
+        g.drawString("Gevulde rechthoek met ovaal",StringX,StringY);
 
         // Taartpunt met ovaal eromheen
-        g.drawOval(480,50,breedte,hoogte);
+        g.drawOval(OvaalX,OvaalY,breedte,hoogte);
         g.setColor(opvulkleur);
-        g.fillArc(480,50,breedte,hoogte,0,45);
+        g.fillArc(TaartPuntX,TaartPuntY,breedte,hoogte,TPStartAngle,TPArcAngle);
         g.setColor(lijnkleur);
-        g.drawString("Taartpunt met ovaal eromheen",500,165);
+        g.drawString("Taartpunt met ovaal eromheen",TPStringX,TPstringY);
 
         // Afgeronde rechthoek
-        g.drawRoundRect(10,200,breedte,hoogte,30,30);
-        g.drawString("Afgeronde rechthoek",50,315);
+        g.drawRoundRect(ARX,ARY,breedte,hoogte,ArcWidth,ArcHeight);
+        g.drawString("Afgeronde rechthoek",StrX,StrY);
 
         // Gevulde ovaal
         g.setColor(opvulkleur);
-        g.fillOval(250,200,breedte,hoogte);
+        g.fillOval(GevuldeOvaalX,GevuldeOvaalY,breedte,hoogte);
         g.setColor(lijnkleur);
-        g.drawString("Gevulde ovaal", 315,315);
+        g.drawString("Gevulde ovaal", StrX1,StrY1);
 
         // Cirkel
-        g.drawOval(525,200,hoogte,hoogte);
-        g.drawString("Cirkel",560,315);
+        g.drawOval(CirkelX,CirkelY,hoogte,hoogte);
+        g.drawString("Cirkel",CirkelStringX,CirkelStringY);
     }
 }
