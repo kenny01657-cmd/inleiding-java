7bc1a04a5ca1263d18ae15edd0614968fc1c52b2
kenny01657-cmd <83949145+kenny01657-cmd@users.noreply.github.com>
2021-05-18 13:26:21 +0200

    Update 6.2 SecIn.java

--- src/h06/6.2 SecIn.java
+++ src/h06/6.2 SecIn.java
@@ -8,17 +8,21 @@ public class SecIn extends Applet {
     int secInUur;
     int secInDag;
     int secInJaar;
+    int x;
+    int y;
 
 
     public void init() {
         secInUur = 60*60;
         secInDag = (60*60)*24;
         secInJaar = ((60*60)*24)*365;
+        x = 20;
+        y = 20;
     }
 
     public void paint(Graphics g) {
-        g.drawString("Seconds in an hour: " + secInUur,20,20);
-        g.drawString("Seconds in a day: " + secInDag,20,35);
-        g.drawString("Seconds in a year: " + secInJaar,20,50);
+        g.drawString("Seconds in an hour: " + secInUur,x,y);
+        g.drawString("Seconds in a day: " + secInDag,x,y+15);
+        g.drawString("Seconds in a year: " + secInJaar,x,y+30);
     }
 }
