2fe213cfbea821578efd07682c8b982c90e04589
kenny01657-cmd <kmeijer@roc-dev.com>
2021-05-19 09:15:23 +0200

    .

---
+++ src/h11/Opdr_Vijf_VierKanten.java
@@ -0,0 +1,26 @@
+package h11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdr_Vijf_VierKanten extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g){
+        // Declareren en Initialiseren
+        int X = 20;
+        int Y = 20;
+        int width = 20;
+        int height = 20;
+
+        // Vijf vierkanten
+        for(int teller = 0; teller < 5; teller++){
+            X += 20;
+            Y += 20;
+            g.drawRect(X,Y,width,height);
+        }
+    }
+
+}
