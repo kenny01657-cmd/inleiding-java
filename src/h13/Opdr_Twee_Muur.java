3e1519f35f2edecf906457702c2774f04607cd53
kenny01657-cmd <kmeijer@roc-dev.com>
2021-06-02 15:09:23 +0200

    .

---
+++ src/h13/Opdr_Twee_Muur.java
@@ -0,0 +1,34 @@
+package h13;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdr_Twee_Muur extends Applet {
+
+    public void init() {
+        setSize(600,300);
+    }
+
+    public void paint(Graphics g) {
+        tekenRodeMuur(g,50,30,50,50);
+    }
+
+    void tekenRodeMuur(Graphics g, int width, int height, int X, int Y){
+        int aantalBakstenen;
+        int X1;
+        X1 = X;
+        aantalBakstenen = 70;
+        for(int i = 0; i < aantalBakstenen; i++){
+            g.setColor(Color.RED);
+            g.fillRect(X,Y,width,height);
+            g.setColor(Color.black);
+            g.drawRect(X,Y,width,height);
+            X += width;
+
+            if(X == X1 + width*10){
+                X -= width*10;
+                Y += height;
+            }
+        }
+    }
+}
